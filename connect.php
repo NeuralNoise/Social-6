24afe8f289ccdf22ef8487504ba58a71a6b30689
Mohammad Moid <moid.mw@gmail.com>
2015-03-20 01:02:02 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "moid";
+$password = "";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=social", $username, $password);
